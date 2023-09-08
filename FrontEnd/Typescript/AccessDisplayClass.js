703dd98d9e1e8771fce9b7b298bfa55fdb871fb6
GR-Saishree <140171987+GR-Saishree@users.noreply.github.com>
2023-09-08 10:44:47 +0530

    Delete FrontEnd/Typescript directory

--- FrontEnd/Typescript/AccessDisplayClass.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var DisplayClassData_1 = require("./DisplayClassData");
-var obj = new DisplayClassData_1.Student(1, 'Sai shree', 21);
-obj.display();
-console.log("\n    Addition of two subject marks is : ".concat((0, DisplayClassData_1.addMarks)(68, 86), ";\n"));
-console.log("Student selected subject is  " + DisplayClassData_1.studentSub);
--- FrontEnd/Typescript/TraverseStringArray.js
+++
@@ -1,7 +0,0 @@
-var strArray = ["This", "is", "day3", "angular", "assignment"];
-strArray.push("create dynamic");
-strArray.push("string array");
-strArray.push("and traverse");
-strArray.forEach(function (String, index) {
-    console.log(String + " ->is found at index " + index);
-});
