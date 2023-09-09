0373a9326f650fa62aa28cdda169faaaae60013c
GR-Saishree <140171987+GR-Saishree@users.noreply.github.com>
2023-09-09 19:27:52 +0530

    Update TradeService.java

--- fxtradingmicroservice/src/main/java/com/springboot/fxtradingmicroservice/service/TradeService.java
+++ fxtradingmicroservice/src/main/java/com/springboot/fxtradingmicroservice/service/TradeService.java
@@ -1,5 +1,6 @@
 package com.springboot.fxtradingmicroservice.service;
 
+// import statements
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +13,11 @@ public class TradeService {
 
 	
 	private static int tradeCount;
-	
+
+	// List which stores booked trade details
 	List<Trade> tradeList = new ArrayList<>();
-	
+
+	// Method to book the trade
 	public boolean bookTrade(Trade trade) {
 		
 		if(!(isValidName(trade.getCustomerName())||isValidCurrencyPair(trade.getCurrencyPair())||isValidAmount(trade.getAmount()))) {
@@ -28,19 +31,22 @@ public class TradeService {
 		}
 	}
 	
-	
+	// Method to get list of trades that have been booked
 	public List<Trade> getTradeList(){
 		return tradeList;
 	}
-	
+
+	// Method for validating customer name
 	private static boolean isValidName(String customerName) {
 		return customerName.matches("^[a-zA-Z. ]+$") && !(customerName.isEmpty());
 	}
-	
+
+	// Method for validating currency pair
 	private static boolean isValidCurrencyPair(String currencyPair) {
 		return currencyPair.equalsIgnoreCase("USDINR") && !(currencyPair.isEmpty());
 	}
-	
+
+	// Method for validating amount
 	private static boolean isValidAmount(double amount) {
 		return amount>0 && amount<= Trade.getMaxTradeValue();
 	}
