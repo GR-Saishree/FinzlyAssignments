642b0c19b7c81d0be30e9f3a172cf0d45f87879a
GR-Saishree <140171987+GR-Saishree@users.noreply.github.com>
2023-09-13 15:52:44 +0530

    Update DoWhileLoop.java

--- JavaLoops/src/com/finzly/java/loops/DoWhileLoop.java
+++ JavaLoops/src/com/finzly/java/loops/DoWhileLoop.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class DoWhileLoop {
 	    
 	        //Question1-Write a program to print 1 to 25 numbers
-			void question1() {
+			void print1To25() {
 				int i=1;
 				do
 				{
@@ -15,7 +15,7 @@ public class DoWhileLoop {
 			}
 			
 			//Question2-Write a program to print 25 to 1 numbers
-			void question2() {
+			void print25To1() {
 				int i=25;
 				do{
 					System.out.print(i+" ");
@@ -26,7 +26,7 @@ public class DoWhileLoop {
 			}
 			
 			//Question3-Write a program to print 1 to 100 odd numbers
-			void question3() {
+			void printOdd1To100() {
 				int i=1;
 				do {
 					if(i%2!=0)
@@ -38,7 +38,7 @@ public class DoWhileLoop {
 			}
 			
 			//Question4-Write a program to print 1 to 100 even numbers
-			void question4() {
+			void printEven1To100() {
 				int i=1;
 				do {
 					if(i%2==0)
@@ -50,7 +50,7 @@ public class DoWhileLoop {
 			}
 				
 			//Question5-Write a program to print sum of 1 to 50 odd numbers
-				void question5() {
+				void printOddSum1To50() {
 					int sum=0;
 					int i=1;
 					do {
@@ -63,7 +63,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question6-Write a program to print sum of 1 to 50 even numbers
-				void question6() {
+				void printEvenSum1To50() {
 					int sum=0;
 					int i=1;
 					do {
@@ -77,7 +77,7 @@ public class DoWhileLoop {
 				}
 				
 			//Question7-Write a program to print -45 t0 +45 numbers
-				void question7() {
+				void printNumbers() {
 					int i=-45;
 					do {
 						System.out.print(i+" ");
@@ -87,7 +87,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question8-Write a program to print 50 t0 100 numbers
-				void question8() {
+				void printNumbers50To100() {
 					int i=50;
 					do {
 						System.out.print(i+" ");
@@ -97,7 +97,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question9-Write a program to print sum of odd and even numbers
-				void question9() {
+				void printSum() {
 					Scanner sc=new Scanner(System.in);
 					System.out.println("Specify start range:");
 					int a=sc.nextInt();
@@ -122,7 +122,7 @@ public class DoWhileLoop {
 					
 				}
 			//Question10-Write a program to print even and odd numbers
-				void question10() {
+				void printEvenOdd() {
 					Scanner sc=new Scanner(System.in);
 					System.out.println("Specify start range:");
 					int a=sc.nextInt();
@@ -144,7 +144,7 @@ public class DoWhileLoop {
 							
 				}
 			//Question11-Write a program to print 1 to 100 numbers
-				void question11() {
+				void print1To100() {
 					int i=1;
 					do{
 						System.out.print(i+" ");
@@ -154,7 +154,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question12-Write a program to print 100 to 1 numbers
-				void question12() {
+				void print100To1() {
 					int i=100;
 					do {
 						System.out.print(i+" ");
@@ -164,7 +164,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question13-Write a program to print 30 to 50 numbers
-				void question13() {
+				void print30To50() {
 					int i=30;
 					do {
 						System.out.print(i+" ");
@@ -174,7 +174,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question14-Write a program to print count of even numbers 1 to 25
-				void question14() {
+				void printEvenCount() {
 					int ecount=0;
 					int i=1;
 					do {
@@ -187,7 +187,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question15-Write a program to print count of odd numbers 1 to 25
-				void question15() {
+				void printOddCount() {
 					int ocount=0;
 					int i=1;
 					do {
@@ -203,7 +203,7 @@ public class DoWhileLoop {
 				
 			// -------------------------------Generate the series---------------------------------------------------
 			//Question16-Generate the series...... 2 4 6 8 10 12 14 16 18 20.
-				void question16() {
+				void evenSeries() {
 					int i=2;
 					do {
 						System.out.print(i+" ");
@@ -213,7 +213,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question17-Generate the series...... 9 18 27 36 45 54 63 72 81 90
-				void question17() {
+				void multiply9Series() {
 					int i=9;
 					do {
 						System.out.print(i+" ");
@@ -223,7 +223,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question18-Generate the series...... 1 -2 3 -4 5 -6 7 -8 9 -10
-				void question18() {
+				void numberSeries() {
 					int i=1;
 					do {
 						if(i%2==0) {
@@ -238,7 +238,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question19-Generate the series...... 5 10 15 20 25 30 35 40 45 50
-				void question19() {
+				void multiply5Series() {
 					int i=5;
 					do {
 						System.out.print(i+" ");
@@ -248,7 +248,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question20-Generate the series...... 1 10 100 1000
-				void question20() {
+				void multiply10Series() {
 					int i=1;
 					do {
 						System.out.print(i+" ");
@@ -258,7 +258,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question21-Generate the series...... 1 3 6 10 15 21 28 36 45
-				void question21() {
+				void previousSumSeries() {
 					int j=1,sum=0,k=1;
 					do {
 						sum=sum+j;
@@ -270,7 +270,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 		    //Question22-Generate the series...... 8 16 24 32 40 48 56 64 72 80
-				void question22() {
+				void multiply8Series() {
 					int i=8;
 					do {
 						System.out.print(i+" ");
@@ -280,7 +280,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question23-Generate the series...... 0 1 1 2 3 5 8 13 21
-				void question23() {
+				void fibonacciSeries() {
 					int a=0,b=1;
 					int sum=0;
 					System.out.print(a+" "+b+" ");
@@ -296,7 +296,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question24-Generate the series...... 1 4 9 16 25 36 49 64 81 100
-				void question24() {
+				void squareSeries() {
 					int i=1;
 					do {
 						System.out.print(i*i+" ");
@@ -306,7 +306,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question25-Generate the series...... 3 6 9 12 15 18 21 24 27 30
-				void question25() {
+				void multiply3Series() {
 					int i=3;
 					do {
 						System.out.print(i+" ");
@@ -316,7 +316,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question26-Generate the series...... 7 14 21 28 35 42 49 56 63 70
-				void question26() {
+				void multiply7Series() {
 					int i=7;
 					do {
 						System.out.print(i+" ");
@@ -326,7 +326,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question27-Generate the series...... 4 8 12 16 20 24 28 32 36 40
-				void question27() {
+				void multiply4Series() {
 					int i=4;
 					do {
 						System.out.print(i+" ");
@@ -336,7 +336,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question28-Generate the series...... 10 20 30 40 50 60 70 80 90 100
-				void question28() {
+				void multiply10Series() {
 					int i=10;
 					do {
 						System.out.print(i+" ");
@@ -346,7 +346,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question29-Generate the series...... 1 2 3 4 5 4 3 2 1
-				void question29() {
+				void printSeries() {
 					int i=1;
 					do {
 						System.out.print(i+" ");
@@ -362,7 +362,7 @@ public class DoWhileLoop {
 					System.out.println("-------------------------------------------------------------------------");
 				}
 			//Question30-Generate the series...... 6 12 18 24 30 36 42 48 54 60
-				void question30() {
+				void multiply6Series() {
 					int i=6;
 					do {
 						System.out.print(i+" ");
